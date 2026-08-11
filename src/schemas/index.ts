18683ab37d1949527ab2a40d974211f461fc90e1
Mhyne <magno.259716@globalcity.sti.edu.ph>
2026-08-11 22:35:28 +0800

    feat: add auth validation schemas

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -6,3 +6,18 @@ export {
   paginationSchema,
   uuidSchema,
 } from "./common";
+export {
+  applicantRegistrationSchema,
+  forgotPasswordSchema,
+  inviteInternalUserSchema,
+  loginSchema,
+  passwordSchema,
+  resetPasswordSchema,
+} from "./auth";
+export type {
+  ApplicantRegistrationInput,
+  ForgotPasswordInput,
+  InviteInternalUserInput,
+  LoginInput,
+  ResetPasswordInput,
+} from "./auth";
