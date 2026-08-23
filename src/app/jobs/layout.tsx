3203768e929dcd8ab47e49f7e8700746bb4f35bc
Mhyne <magno.259716@globalcity.sti.edu.ph>
2026-08-23 22:46:13 +0800

    fix: provide queries to public jobs routes

---
+++ src/app/jobs/layout.tsx
@@ -0,0 +1,7 @@
+import type { ReactNode } from "react";
+
+import { QueryProvider } from "@/components/providers/query-provider";
+
+export default function JobsLayout({ children }: { children: ReactNode }) {
+  return <QueryProvider>{children}</QueryProvider>;
+}
